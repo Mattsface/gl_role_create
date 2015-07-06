3cfea60e5427de50603111131e870040f0d5b2a3
mspah <mspah@zulily.com>
2015-07-06 16:18:20 -0700

    changed application to pull in accountz sls files

--- gl_role_account_create/gl_role_account_create.py
+++ gl_role_account_create/gl_role_account_create.py
@@ -1,36 +1,41 @@
-
 import yaml
 
 
 class CreateRoleAccount:
-    def __init__(self, user_bag, gitlab_key=None):
-        self.user_bag = user_bag
+    def __init__(self, users, ssh, account_name, gitlab_key=None):
+        self.users = users
+        self.ssh = ssh
+        self.account_name = account_name
         self.gitlab_key = gitlab_key
 
-    def import_databag(self):
+    def import_users_sls(self):
         """
-        convert the user_bag to a dictionary
+        convert the users_sls to a dictionary
         """
-        with file(self.user_bag) as f:
-            self.user_bag = yaml.load(f)
+        with file(self.users) as f:
+            self.users = yaml.load(f)
 
+    def import_ssh_sls(self):
+        """
+        convert the ssh_auth.sls to a dictionary
+        """
+        with file(self.ssh) as f:
+            self.ssh = yaml.load(f)
+
+    def convert_to_api(self):
+        """
+        Use self.ssh and self.users to create an object to send to the Gitlab API
+        """
+        pass
+
+        ## TODO: Convert it a form the API can handle
 
     def check_type(self):
         """
         make sure the databag item is a role account
         :return: True or False, if false exceptions
         """
-        pass
 
-
-    def convert_to_api(self):z
-        """
-
-
-        """
-        pass
-
-        ## TODO: Convert it a form the API can handle
 
     def send_to_gitlab(self):
         """
@@ -44,9 +49,18 @@ class CreateRoleAccount:
 
     def verify_create(self):
         """
-
         :return: True or False, if false exception
         """
         pass
         ## TODO: Make sure the account was created
+
+
+
+
+
+
+
+
+
+
 
--- tests/role_account_tests.py
+++ tests/role_account_tests.py
@@ -1,53 +1,107 @@
 import unittest
+import yaml
+from os import remove
 from gl_role_account_create.gl_role_account_create import CreateRoleAccount
+
 
 class TestCreateRoleAccount(unittest.TestCase):
 
     def setUp(self):
-        user1 = {
-            "type": "role",
-            "system": false,
-            "id": "r-vxp-git",
-            "password": "!!",
-            "uid": "534",
-            "gid": ["nobody"],
-            "groups": [],
-            "comment": "VXP git role account,,,",
-            "home": "/home/r-vxp-git",
-            "shell": "/bin/bash",
-            "config_git": "true",
-            "ssh_keys":
-            [
-            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1dT4/LQdxxLntE/CPyxWpqQL+hXSYV6AwdZzULsrhGta2AYEfROPs3pvgXRiYwXSj1USqTCsVcZJ8v/iPp9LaXj8iLGlERWh0bRqijRe53Bw96RvQKJq1ap3VekpjOgHAVOvnXxYz/BHRnr7uzjcYEdm3DHn+5cIB/eWMWdsgvsQ5BLrcITP/u+ofLEayXL+FEC/PexI2C1cXlAz98l9wvB4O0GhgLjQTQDJ1B0g5Cepr/369x5IRNQD/hCX0FHu8O+09tFxiwcWUcHB3MbB4bkuwTvV1Q78xneORwjterpExj5tFeKYKMXL6+h6ewYL5TO/uvvIBmd2TbajJMKMF r-vxp-git@zulily.com"
-            ],
-            "access": [ ]
-        }
-        user2 = {
-            "type": "person",
-            "system": false,
-            "id": "mspah",
-            "password": "$6$Ki.Jd4FB$njmz8j7a0xbop06RcOdbAmp4mgDjsQs6yn05cCjRLJkliTAO6HCAWIl24ChH.VxkY1g36brn7uIzHkpHri1XS1",
-            "uid": "1200",
-            "gid": ["www-coop"],
-            "groups": [],
-            "comment": "Matt Spah,Software Dev Team,,",
-            "home": "/home/mspah",
-            "shell": "/bin/bash",
-            "config_git": "true",
-            "ssh_keys":
-            [
-            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABBwCUrhV7fANUeM1OErZrm/5K193XHEHdcUjxKol/cdijjeUA4mraLMaqga4XU7aU4PVMmgIY7dn8nKP8JOIagxjLXe62/cblnS2Sd2Iv+HghcIENN/k5EXJVAIfOyRpGLlzDe7P4cxeDMlOzTsC9pdxiuYjd4ilh8uLwwpS596AC9CoU229aLva4hZoVrV+/rxR/73vRPqWmBX0lJ+d0MuAZiiwy07+WZquY3bP/CdByzxE7aRRGTXe4vYNIhOSlgj70wPiVlMGziQfaCM4hhLO3qHrxL0WKaSpzIEOM1Y4zpTlo9/NROJrPdwF6iQ3YseIxUkFdk9KJ0vkxeZTSGnV1npbDDOfb mspah@zulily.com"
-            ],
-            "access": ["sysadmin"]
-        }
+        self.users_sls_contents = """
+        accountz_passwd:
+          ahoogen:
+            uid: 10127
+            gid: 10127
+            password_hash: !
+            status: removed
+            shell: /bin/false
+            home_directory: /home/ahoogen
+            full_name: Austen Hoogen
+            team: Software Dev Team
+            pager_email: ""
+            phone_number: ""
+
+          c-rrobeal:
+            uid: 10070
+            gid: 10070
+            password_hash: !
+            status: removed
+            shell: /bin/bash
+            home_directory: /home/c-rrobeal
+            full_name: Rafik Robeal
+            team: Software Dev Contractor
+            pager_email: ""
+            phone_number: ""
+
+          r-infraops-git:
+            uid: 5041
+            gid: 5041
+            password_hash: !
+            status: active
+            shell: /bin/bash
+            home_directory: /home/r-infraops-git
+            full_name: InfraOps git
+            team: ""
+            pager_email: ""
+            phone_number: ""
+        """
+        self.ssl_auth_contents = """
+        accountz_ssh_auth:
+          aargo:
+            - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCt1nKLrEoea0mfFDgT1uxL4JxSB03U5ooklpSDMWxARzmaVjSDcQ3CyS/v2tELPaboCUpsu9qIZg6bep3q9pWGhOnlD4uY3zWUJmU4K7p01HCX+/JFdUCHosWRHJVsZM9/OutLINU6npztSaCEQRSmIfSpIOTCaIgJxliIUBDkGNgfakWsTzwoCLjbD8PDzSchJu2bmTnxJrAdB8wlcsuMQvakN0Ej3BdKIvdylahh01UfuiSSGwmjgovSKiVW0HAc5GZ9qZFKIclBaBs2k0xaMaP/6ojLngszKOnT2whHQGNC4YWKWsKueT/GQcVfQGv9IEKAI6hNuRcwBOfukvdH aargo@zulily.com
+
+          adbarrett:
+            - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKGp/JDHjvNU3RMH7GIZsnEevptWN02l5WNSpSQW2B5vgdk/lasAOD3B1bvFOAPimu1l7h39xE+tzcmZFCk1ERAR1r9la5/xIVKeZlwxesRo5dogv1q3orJPtAgim5CxYHftkybgOVdpXT7RFzXAa1Zj2HWs46wM0cTreX0xpSrDtIVECZlLYOvJbu1UkNvW8flcnnJrohswsVakDTTt29opOIw/8FqVelw9T+jguiPW9ywrFU+/EuJUiTTCWzchwy+0kI/EBC7w0x8qmNwJIsNCw3Hq/sgMN8prtEUb8ti61j9+aEsMrjH/Z3rymoeaQWnLAsKs67bavSIUIaVPzh adbarrett@zulily.com
+
+          r-infraops-git:
+            - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCJlrG43+2eJDNi3pkIph8PHCqJAoTRq1sRVZZ9tW0AXSXROOcxB1wLHFIx9iiBKNKqrxSRmFyKaWtvMjSqxL7e62Ll+QfjQGt8UyyiTVfWFx+aGy8YUil3OPej7UayOS3Izk6zwDUvRAjP41kIdZEP8oRTvRbUdo3j8sLIaSkF/yiYtqehSl2yZasQAOubLBbk4hq5sgLfmdyScb6J+zRn5SEwMHNufTbPQXArAeZa+zVA4y/zrCecPRIKfWCi6JadaZRS7DPa2Eyj1ShLVDqD7vIT4EyhAIkPFXam+MK641DyoNj9HSHv5FaAt6fSvQaH9PR/MCSSSdwgb6YT/Jx r-infraops-git@zulily.com
+        """
+
+        self.ssl_auth_filename = 'ssh_auth.sls'
+        self.users_filename = 'users.sls'
+
+        with file(self.users_filename, 'w') as f:
+            f.write(self.users_sls_contents)
+        with file(self.ssl_auth_filename, 'w') as f:
+            f.write(self.ssl_auth_contents)
+
+    def tearDown(self):
+        remove(self.ssl_auth_filename)
+        remove(self.users_filename)
+
+    def test_import_users_sls(self):
+        """
+        Testing the import_users_sls() function
+        :return: dictionary
+        """
+        user = 'r-infraops-git'
+        role_user = CreateRoleAccount(self.users_filename, self.ssl_auth_filename, user)
+        expected = dict
+        role_user.import_users_sls()
+        actual = type(role_user.users)
+        self.assertEqual(expected, actual, "Expected type {}. but got {},".format(expected, actual))
+
+    def test_failed_users_sls(self):
+        """
+        Testing a failed import_users_sls() function
+        :return:
+        """
+        pass
 
 
-    def test_import_databag(self):
+    def test_import_ssh_sls(self):
+        """
+        Testing the import_ssh_sls function
+        :returns account.check_type returns True if databag is a role account
+        """
+        user = 'r-infraops-git'
+        role_user = CreateRoleAccount(self.users_filename, self.ssl_auth_filename, user)
+        expected = dict
+        role_user.import_ssh_sls()
+        actual = type(role_user.ssh)
+        self.assertEqual(expected, actual, "Expected type {}. but got {},".format(expected, actual))
 
-        # This isn't going to work, change set up to create tempory files and teardown to remove them
-        account = CreateRoleAccount(user1)
-
-    def test_failed_import_databag(self):
+    def test_failed_import_ssh_sls(self):
         pass
 
     def test_dict_creation(self):
