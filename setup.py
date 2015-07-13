866c44627e0556b7aac6a69269493a49989af514
mspah <mspah@zulily.com>
2015-07-13 12:52:21 -0700

    Created package skeleton

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(name='gl_role_account_create',
       description='CLI to create a Gitlab Role account from a Role account user data bag item',
       author='Matthew Spah',
       author_email='spahmatther@gmail.com',
-      scripts=[''],
+      scripts=['gl_role_account_create/bin/create_role.py'],
       packages=find_packages(),
-      install_requires=[],
+      install_requires=['pyyaml',
+                        'python-gitlab'],
+      dependency_links=[
+          "git+ssh://git@github.com:gpocentek/python-gitlab.git#python-gitlab"
+      ],
       zip_safe=False)
