4a51edc0d86c64362a30d3ba0f25a20f3f4b65d3
mspah <mspah@zulily.com>
2015-08-02 23:29:13 -0700

    Added argument for config.ini, and added the argument to to import_config

--- create_gl_role/bin/create_role.py
+++ create_gl_role/bin/create_role.py
@@ -13,7 +13,7 @@ from gitlab import *
 
 def main():
     args = parse_arguments()
-    config = import_config()
+    config = import_config(args.config_file)
 
     pillar_periodic = config.get('repo', 'pillar_periodic')
     gitlab_token = config.get('gitlab', 'token')
@@ -30,8 +30,11 @@ def main():
     else:
         password = generate_random_password(args.random_password_length)
 
-
-    assert os.path.isdir(pillar_periodic)
+    try:
+        assert os.path.isdir(pillar_periodic)
+    except:
+        print "Unable to located pillar_periodic directory"
+        sys.exit(1)
 
     try:
         repo = git.cmd.Git(pillar_periodic)
@@ -39,6 +42,7 @@ def main():
     except:
         e = sys.exc_info()[0]
         print "Failed to pull down changes to pillar_periodic, error: {}".format(e)
+        sys.exit(1)
 
     ssh_path = os.path.join(pillar_periodic, "core/accountz/ssh_auth.sls")
     user_path = os.path.join(pillar_periodic, "core/accountz/users.sls")
@@ -66,22 +70,25 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser()
     password_group = parser.add_mutually_exclusive_group(required=True)
-    password_group.add_argument('-r', action='store', dest='random_password_length', help="Length of random password to be created")
+    password_group.add_argument('-r', action='store', dest='random_password_length', type=int, help="Length of random password to be created")
     password_group.add_argument('-p', action='store', dest='password', help="Password for role account")
     parser.add_argument('-a', action='store', dest='account_name', required=True, help="Name of the role account you want to create")
     parser.add_argument('-e', action='store', dest='account_email', help="Role account email, defaults to account_name@zulily.com")
+    parser.add_argument('-c', action='store', dest='config_file', help="Location of config.ini")
 
     args = parser.parse_args()
     return args
 
 
-def import_config():
+def import_config(config_ini=None):
     """
     import config file
     """
     config = ConfigParser.ConfigParser()
     user = getpass.getuser()
-    config_ini = "/home/{}/.create_role/config.ini".format(user)
+
+    if config_ini is None:
+        config_ini = "/home/{}/.create_role/config.ini".format(user)
     try:
         config.readfp(open(config_ini))
         return config
